044ea65e62e47f2710c20d726c894c86b1c40bca
UladzislauValchkou <tarantino459@gmail.com>
2018-05-06 02:36:46 +0300

    5 task

--- uladzislau_valchkou_l5.rb
+++ uladzislau_valchkou_l5.rb
@@ -1,35 +1,22 @@
 #!/usr/bin/env ruby
 # top level comment
-class Script
-  def self.run(&block)
-    @result = yield(block)
-  end
-end
-# top level comment
 class SuperScript < Script
-  def self.write(way, text)
-    f = File.open(way, 'a')
-    f.puts(text)
-    f.close
-  end
-
-  def self.run(name, stdout_log = nil, stderr_log = nil, &block)
-    super(&block)
+  def self.run(name, _stdout_log = nil, _stderr_log = nil)
+    super()
+    result = yield
   rescue StandardError => err
-    err_log = "ERROR: #{Time.now} #{name} #{err.message}}"
-    stderr_log ? write(stderr_log, err_log) : (p err_log)
+    err_log = "ERROR: #{Time.now} #{name[:name]} #{err}"
+    if name[:stderr_log].nil?
+      puts err_log
+    else
+      File.open(name[:stderr_log], 'w') { |f| f.puts err_log }
+    end
   else
-    out_log = "#{Time.now} #{name} #{@result}"
-    stdout_log ? write(stdout_log, out_log) : (p out_log)
+    out_log = "#{Time.now} #{name[:name]} #{result}"
+    if name[:stdout_log].nil?
+      puts out_log
+    else
+      File.open(name[:stdout_log], 'w') { |f| f.puts out_log }
+    end
   end
-end
-
-SuperScript.run('mtn', './my.log', './ERR.log') do
-  a = 1
-  a += 5
-  a
-end
-
-SuperScript.run('mtn', './my.log', './ERR.log') do
-  1 / 0
 end
